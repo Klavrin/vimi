30df4bf271f48304c4b6fa52c91b26f50d1e1f38
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2024-05-10 23:50:12 +0300

    Improve the performance of unified processing

--- src/renderer/components/markdown-preview.tsx
+++ src/renderer/components/markdown-preview.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import { createElement, Fragment, useEffect, useState } from 'react';
+import * as prod from 'react/jsx-runtime';
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
-import remarkGfm from 'remark-gfm';
-import remarkReact from 'remark-react';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import rehypeReact from 'rehype-react';
 
 import StyledPreviewMarkdown from './styles/preview-markdown.styled';
 
@@ -11,9 +13,12 @@ type MarkdownPreviewProps = {
   previewMode: boolean;
 };
 
+type Production = { Fragment: any; jsx: any; jsxs: any };
+const production = { Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs };
+
 function MarkdownPreview({ innerText, previewMode }: MarkdownPreviewProps) {
-  const [md, setMd] = useState('');
-  const [timeoutId, setTimeoutId] = useState(null);
+  const [md, setMd] = useState(createElement(Fragment));
+  const [timeoutId, setTimeoutId] = useState<any>(null);
 
   useEffect(() => {
     if (timeoutId) {
@@ -21,23 +26,29 @@ function MarkdownPreview({ innerText, previewMode }: MarkdownPreviewProps) {
       setTimeoutId(null);
     }
 
-    setTimeout(() => {
-      const markdown: any = unified()
-        .use(remarkParse)
-        .use(remarkGfm)
-        // @ts-ignore
-        .use(remarkReact, React)
-        .processSync(innerText).result;
+    const timeout = setTimeout(() => {
+      (() => {
+        (async () => {
+          const processor: any = unified()
+            .use(remarkParse)
+            .use(remarkRehype)
+            .use(rehypeStringify)
+            .use(rehypeReact, production as Production)
+            .processSync(innerText);
 
-      setMd(markdown);
+          setMd(processor.result);
+        })();
+      })();
     }, 400);
+
+    setTimeoutId(timeout);
 
     return () => {
       if (timeoutId) {
         clearTimeout(timeoutId);
       }
     };
-  }, [previewMode, innerText, timeoutId]);
+  }, [innerText]); // ?
 
   return (
     <StyledPreviewMarkdown style={{ display: previewMode ? 'block' : 'none' }}>
