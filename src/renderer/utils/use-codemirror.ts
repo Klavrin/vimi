7b962f76dccea45e766d79f4f18582bd146c1ccb
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2024-05-19 19:34:02 +0300

    Add syntax highlighting

--- src/renderer/utils/use-codemirror.ts
+++ src/renderer/utils/use-codemirror.ts
@@ -2,10 +2,62 @@ import { useState, useEffect, useRef } from 'react';
 import { EditorState } from '@codemirror/state';
 import { EditorView, keymap, lineNumbers } from '@codemirror/view';
 import { defaultKeymap, history, historyKeymap } from '@codemirror/commands';
-import { indentOnInput, bracketMatching } from '@codemirror/language';
-import { javascript } from '@codemirror/lang-javascript';
+import {
+  indentOnInput,
+  bracketMatching,
+  // defaultHighlightStyle,
+  HighlightStyle,
+  syntaxHighlighting,
+} from '@codemirror/language';
+import { tags } from '@lezer/highlight';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data';
+// import { javascript } from '@codemirror/lang-javascript';
 
 import { vim } from '@replit/codemirror-vim';
+
+const customSyntaxHighlighting = HighlightStyle.define([
+  {
+    tag: tags.heading1,
+    fontSize: '1.8em',
+    fontWeight: 'bold',
+  },
+  {
+    tag: tags.heading2,
+    fontSize: '1.6em',
+    fontWeight: 'bold',
+  },
+  {
+    tag: tags.heading3,
+    fontSize: '1.4em',
+    fontWeight: 'bold',
+  },
+  {
+    tag: tags.heading4,
+    fontSize: '1.2em',
+    fontWeight: 'bold',
+  },
+  {
+    tag: tags.heading5,
+    fontSize: '1em',
+    fontWeight: 'bold',
+  },
+  {
+    tag: tags.heading6,
+    fontSize: '0.8em',
+    fontWeight: 'bold',
+  },
+  {
+    tag: tags.emphasis,
+    fontStyle: 'italic',
+    color: '#318CE7',
+  },
+  {
+    tag: tags.strong,
+    fontWeight: 'bold',
+    color: '#F5761A',
+  },
+]);
 
 type onChangeProps = (value: EditorState) => void;
 
@@ -25,7 +77,12 @@ function useCodemiror(doc: string, onChange?: onChangeProps) {
         lineNumbers(),
         indentOnInput(),
         bracketMatching(),
-        javascript(),
+        // javascript(),
+        markdown({
+          base: markdownLanguage,
+          codeLanguages: languages,
+        }),
+        syntaxHighlighting(customSyntaxHighlighting),
         EditorView.lineWrapping,
         EditorView.updateListener.of((update) => {
           if (update.changes) {
