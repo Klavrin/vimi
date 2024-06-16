689bdffc55a82d4265e5cd96b0b19abfe609f2eb
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2024-06-16 13:48:55 +0300

    refactor: add class names to the customSyntaxHighlighting in the use-codemirror hook instead of applying css properties directly

--- src/renderer/utils/use-codemirror.ts
+++ src/renderer/utils/use-codemirror.ts
@@ -1,124 +1,76 @@
 import { useState, useEffect, useRef } from 'react';
 import { EditorState } from '@codemirror/state';
-import { EditorView, keymap, lineNumbers } from '@codemirror/view';
+import { EditorView, keymap } from '@codemirror/view';
 import { defaultKeymap, history, historyKeymap } from '@codemirror/commands';
 import {
   indentOnInput,
   bracketMatching,
-  // defaultHighlightStyle,
   HighlightStyle,
   syntaxHighlighting,
 } from '@codemirror/language';
 import { tags } from '@lezer/highlight';
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 import { languages } from '@codemirror/language-data';
-// import { javascript } from '@codemirror/lang-javascript';
 
 import { vim } from '@replit/codemirror-vim';
 
 const customSyntaxHighlighting = HighlightStyle.define([
   {
     tag: tags.heading1,
-    fontSize: '1.8em',
-    fontWeight: 'bold',
-    lineHeight: '30px',
+    class: 'cm-heading1',
   },
   {
     tag: tags.heading2,
-    fontSize: '1.6em',
-    fontWeight: 'bold',
+    class: 'cm-heading2',
   },
   {
     tag: tags.heading3,
-    fontSize: '1.4em',
-    fontWeight: 'bold',
+    class: 'cm-heading3',
   },
   {
     tag: tags.heading4,
-    fontSize: '1.2em',
-    fontWeight: 'bold',
+    class: 'cm-heading4',
   },
   {
     tag: tags.heading5,
-    fontSize: '1em',
-    fontWeight: 'bold',
+    class: 'cm-heading5',
   },
   {
     tag: tags.heading6,
-    fontSize: '0.8em',
-    fontWeight: 'bold',
+    class: 'cm-heading6',
   },
   {
     tag: tags.emphasis,
-    fontStyle: 'italic',
-    color: '#318CE7',
+    class: 'cm-emphasis',
   },
   {
     tag: tags.strong,
-    fontWeight: 'bold',
-    color: '#F5761A',
+    class: 'cm-strong',
   },
   {
-    tag: [
-      // tags.name,
-      // tags.deleted,
-      // tags.character,
-      tags.propertyName,
-      tags.attributeName,
-      // tags.macroName,
-    ],
-    color: '#4078f2',
-  },
-  {
-    tag: [tags.comment, tags.string],
-    color: '#50a14f',
+    tag: [tags.attributeName, tags.variableName],
+    class: 'cm-def',
   },
   {
     tag: tags.keyword,
-    color: '#007acc',
+    class: 'cm-keyword',
   },
   {
-    tag: tags.typeName,
-    color: '#f08d49',
+    tag: tags.comment,
+    class: 'cm-comment',
   },
-
-  // { tag: tags.keyword, color: 'violet' },
-  // {
-  //   tag: [tags.name, tags.deleted, tags.character, tags.propertyName, tags.macroName],
-  //   color: 'red', // this is for the thing
-  // },
-  // { tag: [tags.function(tags.variableName), tags.labelName], color: 'red' },
-  // { tag: [tags.color, tags.constant(tags.name), tags.standard(tags.name)], color: 'whyskey' },
-  // { tag: [tags.definition(tags.name), tags.separator], color: 'blue' },
-  // {
-  //   tag: [
-  //     tags.typeName,
-  //     tags.className,
-  //     tags.number,
-  //     tags.changed,
-  //     tags.annotation,
-  //     tags.modifier,
-  //     tags.self,
-  //     tags.namespace,
-  //   ],
-  //   color: 'tomato',
-  // },
-  // {
-  //   tag: [
-  //     tags.operator,
-  //     tags.operatorKeyword,
-  //     tags.url,
-  //     tags.escape,
-  //     tags.regexp,
-  //     tags.link,
-  //     tags.special(tags.string),
-  //   ],
-  //   color: 'cyan',
-  // },
-  // { tag: [tags.meta, tags.comment], color: 'brown' },
-  // { tag: [tags.atom, tags.bool, tags.special(tags.variableName)], color: 'lightblue' },
-  // { tag: [tags.processingInstruction, tags.string, tags.inserted], color: 'green' },
-  // { tag: tags.invalid, color: 'invalid' },
+  {
+    tag: tags.variableName,
+    class: 'cm-variable',
+  },
+  {
+    tag: tags.operator,
+    class: 'cm-operator',
+  },
+  {
+    tag: tags.propertyName,
+    class: 'cm-property',
+  },
 ]);
 
 type onChangeProps = (value: EditorState) => void;
