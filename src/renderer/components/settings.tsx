06f53c8f46890aa77fe13110fa470273c005930a
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-04-04 12:00:57 +0300

    feat: add settings component

---
+++ src/renderer/components/settings.tsx
@@ -0,0 +1,7 @@
+import StyledSettings from './styles/settings.styled';
+
+const Settings = () => {
+  return <StyledSettings></StyledSettings>;
+};
+
+export default Settings;
---
+++ src/renderer/components/styles/settings.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledSettings = styled.div`
+  width: 100rem;
+  height: 100rem;
+  backdrop-filter: blur(10px);
+`;
+
+export default StyledSettings;
