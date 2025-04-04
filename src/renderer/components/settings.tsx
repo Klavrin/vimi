6a030b82ecac4277ebdad76d6e47e00d08f3b967
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-04-04 12:33:26 +0300

    feat: add in and out animation to settings view

--- src/renderer/components/settings.tsx
+++ src/renderer/components/settings.tsx
@@ -1,6 +1,7 @@
 import StyledSettings from './styles/settings.styled';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleSettings } from '../store/reducers/workspace';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import { State } from '../types/state';
 
@@ -11,13 +12,22 @@ const Settings = () => {
   const dispatch = useDispatch();
 
   return (
-    settingsVisible && (
-      <StyledSettings onClick={() => dispatch(toggleSettings())}>
-        <div className="container" onClick={(e) => e.stopPropagation()}>
-          settings view
-        </div>
-      </StyledSettings>
-    )
+    <AnimatePresence>
+      {settingsVisible && (
+        <StyledSettings
+          onClick={() => dispatch(toggleSettings())}
+          as={motion.div}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.15 }}
+        >
+          <div className="container" onClick={(e) => e.stopPropagation()}>
+            settings view
+          </div>
+        </StyledSettings>
+      )}
+    </AnimatePresence>
   );
 };
 
